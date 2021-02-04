4a07508b9b5ca67876c1e5c6031ad7cdccdeb9b2
Aaron DeRuvo <>
2021-02-03 16:21:28 -0800

    mobile stars

--- src/home/Cover.tsx
+++ src/home/Cover.tsx
@@ -2,11 +2,14 @@
 import {jsx, css, keyframes} from "@emotion/core"
 import CoverContent from "src/home/CoverContent"
 import { colors } from "src/styles"
-import { DESKTOP_BREAKPOINT, HEADER_HEIGHT } from 'src/shared/Styles'
-import celoAsStars from "src/home/celo-galaxy.svg"
+import { DESKTOP_BREAKPOINT, HEADER_HEIGHT, TABLET_BREAKPOINT } from 'src/shared/Styles'
+import celoAsStarsMobile from "src/home/celo-sky-mobile.svg"
+import celoAsStarsTablet from "src/home/celo-sky-tablet.svg"
+import celoAsStarsDesktop from "src/home/celo-sky-desktop.svg"
+
 import examplePhones from "src/home/app-examples@2x.png"
 import Stats from "./stats/Stats"
-import { flex, WHEN_DESKTOP } from "src/estyles"
+import { flex, WHEN_DESKTOP, WHEN_MOBILE, WHEN_TABLET } from "src/estyles"
 import { useScreenSize } from "src/layout/ScreenSize"
 import { useState } from "react"
 import { NameSpaces, useTranslation } from "src/i18n"
@@ -24,7 +27,13 @@ export default function Cover() {
   const {t} = useTranslation(NameSpaces.home)
   return (
     <div css={rootCss}>
-      <img src={celoAsStars} alt=""   css={backgroundArea}/>
+      <picture>
+        <source media={`(max-width: ${TABLET_BREAKPOINT}px)`} srcSet={celoAsStarsMobile}/>
+        <source media={`(max-width: ${DESKTOP_BREAKPOINT}px)`} srcSet={celoAsStarsTablet}/>
+        <img  src={celoAsStarsDesktop}
+          alt=""
+          css={backgroundArea}  />
+      </picture>
       <div css={useableArea}>
         <CoverContent />
         {(isDesktop || isTablet) && <picture>
@@ -42,16 +51,19 @@ const rootCss = css(flex,{
   alignSelf: "center",
   alignItems: "center",
   backgroundColor: colors.dark,
-  marginTop: HEADER_HEIGHT,
   paddingTop: HEADER_HEIGHT,
   width: "100%",
-  maxWidth: "100vw"
+  maxWidth: "100vw",
+  [WHEN_MOBILE]: {
+    justifyContent: "center",
+    minHeight: "calc(100vh - 24px)",
+  }
 })
 
 const starKeyFrames =  keyframes`
   from {
     opacity: 0.1;
-    transform: scale(0.9);
+    transform: scale(1.01);
   }
 
   25% {
@@ -62,23 +74,28 @@ const starKeyFrames =  keyframes`
     opacity: 1;
     transform: scale(1);
   }
-
 `
+
 
 const backgroundArea = css({
   position: "absolute",
-  display: "none",
   animationIterationCount: 1,
   animationFillMode: "both",
-  animationDelay: "1s",
-  animationDuration: "20s",
+  animationDelay: "100ms",
+  animationDuration: "10s",
   animationName: starKeyFrames,
   animationTimingFunction: "ease-in-out",
   opacity: 0.1,
+  width: '100%',
+  height: "100%",
+  left: 0,
+  right: 0,
+  top: 50,
+  [WHEN_MOBILE]: {
+    height: "100vh",
+  },
   [WHEN_DESKTOP]: {
-    display: "block",
     maxWidth: 1380,
-    width: '100%',
     height: 920,
   }
 })
