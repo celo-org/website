3351b0e7fbf620550be7a78fe3520e7d5fae7d4b
Henry Nunez <henry.nunez@clabs.co>
2021-02-04 14:49:53 -0500

    henrynunez112/almost dones just waiting on links for HomeBuild.tsx

--- src/home/HomeBuild.tsx
+++ src/home/HomeBuild.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
-import { StyleSheet, View } from 'react-native'
 import { H1 } from 'src/fonts/Fonts'
 import { Adventure } from 'src/home/Adventure'
 import blankSquare from 'src/icons/Rectangle.png'
 import { NameSpaces, useTranslation } from 'src/i18n'
 import { Cell, GridRow, Spans } from 'src/layout/GridRow'
-import Button, { BTN, SIZE } from 'src/shared/Button.3'
 import { standardStyles, textStyles} from 'src/styles'
 
 export default function HomeBuild() {
@@ -30,22 +28,12 @@ export default function HomeBuild() {
         allStyle={standardStyles.blockMarginTopMobile}
         mobileStyle={standardStyles.sectionMarginBottomMobile}
         >
-            <Adventure source={blankSquare} title={t('build1.title')} text={t('build1.text')} link={t('build1.link')}/> 
-            <Adventure source={blankSquare} title={t('build2.title')} text={t('build2.text')} link={t('build2.link')}/>
-            <Adventure source={blankSquare} title={t('build3.title')} text={t('build3.text')} link={t('build3.link')}/>
+            <Adventure source={blankSquare} title={t('build1.title')} text={t('build1.text')} link={{ href: 'https://valoraapp.com', text: t('build1.link')}}/> 
+            <Adventure source={blankSquare} title={t('build2.title')} text={t('build2.text')} link={{ href: 'https://valoraapp.com/3l5XLtC', text: t('build2.link')}}/>
+            <Adventure source={blankSquare} title={t('build3.title')} text={t('build3.text')} link={{ href: 'https://valoraapp.com', text: t('build3.link')}}/>
 
         </GridRow>
         </>
     )
 }
 
-const styles = StyleSheet.create({
-    links: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly',
-        flexWrap: 'wrap',
-    },
-    link:{ 
-        padding: 10,
-    }
-})
