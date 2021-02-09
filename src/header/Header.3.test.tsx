824da87eb98b025297449928e0bbbc536cb95c66
Henry Nunez <henry.nunez@clabs.co>
2021-02-09 10:17:29 -0500

    header testing

--- src/header/Header.3.test.tsx
+++ src/header/Header.3.test.tsx
@@ -1,11 +1,17 @@
-import { fireEvent, render, waitFor } from '@testing-library/react'
-import { fromPromise } from 'apollo-boost'
 import * as React from 'react'
+import * as renderer from 'react-test-renderer'
 import { TestProvider } from 'src/_page-tests/test-utils'
-import Header from 'src/header/Header.3'
+import Header from './Header.3'
 
-describe("<Header />", () =>{
-    test("this should show the header", async () =>{
-
+describe("Header", () =>{
+    it("renders", () =>{
+        const tree = renderer
+        .create(
+            <TestProvider>
+                <Header />
+            </TestProvider>
+        )
+        .toJSON()
+        expect(tree).toMatchSnapshot()
     })
 })
