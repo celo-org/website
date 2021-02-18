5e76c4a798f7904bdfe0ec59e59eeb21c7ee6b10
Aaron DeRuvo <>
2021-02-17 16:15:13 -0800

    it is what it s

--- src/header/Navigation.test.tsx
+++ src/header/Navigation.test.tsx
@@ -2,16 +2,7 @@ import * as React from 'react'
 import * as renderer from 'react-test-renderer'
 import { TestProvider } from 'src/_page-tests/test-utils'
 import Navigation from './Navigation'
-import { render } from '@testing-library/react'
-const {withRouter} = jest.requireMock("next/router")
-jest.mock("next/router", () => {
-    return {
-        useRouter: () => ({
-            pathname: "/"
-        }),
-        withRouter
-    }
-})
+
 describe("Navigation", () =>{
     it("renders", () =>{
         const tree = renderer
@@ -22,13 +13,5 @@ describe("Navigation", () =>{
         )
         .toJSON()
         expect(tree).toMatchSnapshot()
-    })
-
-    describe('when', () => {
-        it("", () => {
-                render(<TestProvider>
-                    <Navigation />
-                </TestProvider>)
-        })
     })
 })
