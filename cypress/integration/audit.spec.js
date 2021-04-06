2ea10d5fd0cb5b41e4b7ebf865a5170bbf3cf13e
MuckT <mcgtom10@gmail.com>
2021-04-06 09:23:46 -0700

    test: add cypress audit tests - lighthouse and pa11y

---
+++ cypress/integration/audit.spec.js
@@ -0,0 +1,42 @@
+describe('Audits', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
+
+  it('should pass lighthouse audit (desktop)', () => {
+    // TODO: Determine appropriate levels
+    cy.lighthouse(
+      {
+        accessibility: 100,
+        'best-practices': 100,
+        seo: 100,
+        pwa: 100,
+      },
+      {
+        formFactor: 'desktop',
+        screenEmulation: {
+          mobile: false,
+          width: 1920,
+          height: 1080,
+          deviceScaleFactor: 1,
+          disabled: false,
+        },
+      }
+    );
+  });
+
+  it('should pass lighthouse audit (mobile)', () => {
+    cy.lighthouse(
+      {
+        accessibility: 100,
+        'best-practices': 100,
+        seo: 100,
+        pwa: 100
+      }
+    );
+  });
+
+  it('should pass pa11y audit', () => {
+    cy.pa11y();
+  });
+});
