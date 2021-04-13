6f2dad5a3eec48cdb433a8ca4ccc81e1ba5ad201
MuckT <mcgtom10@gmail.com>
2021-04-13 13:09:58 -0700

    test: set mobile viewport as default

--- cypress/integration/audit.spec.js
+++ cypress/integration/audit.spec.js
@@ -1,42 +1,46 @@
+/// <reference types="cypress" />
+
 describe('Audits', () => {
-  beforeEach(() => {
-    cy.visit('');
-  });
-
-  it('should pass lighthouse audit (desktop)', () => {
-    // TODO: Determine appropriate levels
-    cy.lighthouse(
-      {
-        accessibility: 100,
-        'best-practices': 100,
-        seo: 100,
-        pwa: 100,
-      },
-      {
-        formFactor: 'desktop',
-        screenEmulation: {
-          mobile: false,
-          width: 1920,
-          height: 1080,
-          deviceScaleFactor: 1,
-          disabled: false,
+  describe('Celo.org HomePage', () => {
+    beforeEach(() => {
+      cy.visit('');
+    });
+  
+    it('should pass lighthouse audit (desktop)', () => {
+      // TODO: Determine appropriate levels
+      cy.lighthouse(
+        {
+          accessibility: 100,
+          'best-practices': 100,
+          seo: 100,
+          pwa: 100,
         },
-      }
-    );
-  });
-
-  it('should pass lighthouse audit (mobile)', () => {
-    cy.lighthouse(
-      {
-        accessibility: 100,
-        'best-practices': 100,
-        seo: 100,
-        pwa: 100
-      }
-    );
-  });
-
-  it('should pass pa11y audit', () => {
-    cy.pa11y();
+        {
+          formFactor: 'desktop',
+          screenEmulation: {
+            mobile: false,
+            width: 1920,
+            height: 1080,
+            deviceScaleFactor: 1,
+            disabled: false,
+          },
+        }
+      );
+    });
+  
+    it('should pass lighthouse audit (mobile)', () => {
+      cy.lighthouse(
+        {
+          accessibility: 100,
+          'best-practices': 100,
+          seo: 100,
+          pwa: 100
+        }
+      );
+    });
+  
+    it('should pass pa11y audit', () => {
+      cy.pa11y();
+    });
   });
 });
