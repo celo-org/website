1df5508d8eb29c5109de8c0b6045b247dbc9f41b
Aaron DeRuvo <>
2021-04-15 09:48:19 -0700

    fix type issues

--- src/lib.dom.d.ts
+++ src/lib.dom.d.ts
@@ -1,4 +1,13 @@
 // only in chrome / opera
 interface Navigator {
   deviceMemory?: 0.25 | 0.5 | 1 | 2 | 4 | 8
+  connection?: any
+  mozConnection?: any
+  webkitConnection?: any
+}
+
+declare namespace React {
+  interface HTMLAttributes {
+    css?: any
+  }
 }
