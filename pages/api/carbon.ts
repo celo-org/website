2d8810fa2cc4de433dc0627148a56b02f34255a7
Henry Nunez <henry.nunez@clabs.co>
2021-04-20 17:33:21 -0400

    it was co not com

--- pages/api/carbon.ts
+++ pages/api/carbon.ts
@@ -1,14 +1,13 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import {wrenApi} from 'server/wrenApi'
-
 import respondError from 'server/respondError'
+
 export default async function(req: NextApiRequest, res: NextApiResponse){
 
     try{
         if(req.method === 'GET'){
             const results = await wrenApi()
-            res.json(results)
-            console.log("results", results)
+            res.json(results.stats)
         }else{
             res.status(405)
         }
--- server/wrenApi.ts
+++ server/wrenApi.ts
@@ -2,14 +2,13 @@ import getConfig from 'next/config'
 
 export async function wrenApi (){
     const { serverRuntimeConfig } = getConfig()
-    const  res = await fetch(`https://www.wren.com/api/offset-orders`, {
+    const  res = await fetch(`https://www.wren.co/api/offset-orders`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${serverRuntimeConfig.WREN_API}`
         },
     })
-    console.log(res)
     return res.json()
     
 }
