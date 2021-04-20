408cd6d31db497561ba6b486e7588bbb5202e6f1
Henry Nunez <henry.nunez@clabs.co>
2021-04-20 14:57:56 -0400

    front end almost done

--- src/home/stats/CarbonStats.tsx
+++ src/home/stats/CarbonStats.tsx
@@ -1,17 +1,18 @@
 import { useAsync } from 'react-async-hook'
+import { NameSpaces, useTranslation } from 'src/i18n'
 import { Datum } from './Stats'
-
-
-
-
-
 
 async function fetchCarbs() {
     const res = await fetch('/api/carbon')
     return res.json()
 }
 
-export function CarbonDocs(){
-    const treesSaved = useAsync(fetchCarbs, [])
-       return <Datum value={`2219.5 tons`} title={`Carbon Offset`} id="2219.5 tons" />
+export default function CarbonDocs(){
+    const {t} = useTranslation(NameSpaces.home)
+    const carbonOffset = useAsync(fetchCarbs, [])
+    if(!carbonOffset.result?.carbon){
+        return null
+    }
+
+       return <Datum value={`${carbonOffset.result?.carbon} tons`} title={t("statsCo2Offset")} id="carbon" />
 }
