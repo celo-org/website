ee73e1fa41f40559003bd0b534470945f40a38e6
Henry Nunez <henry.nunez@clabs.co>
2021-04-20 13:00:08 -0400

    a little more cleaning

--- src/home/stats/CarbonStats.tsx
+++ src/home/stats/CarbonStats.tsx
@@ -12,36 +12,7 @@ async function fetchCarbs() {
 }
 
 export function CarbonDocs(){
-    // const treesSaved = useAsync(fetchCarbs, [])
+    const treesSaved = useAsync(fetchCarbs, [])
 
-    // if(!treesSaved.result){
-    //     return (
-    //         <div>
-
-    //         </div>
-    //     )
-    // }
-
-   /** if(treesSaved.result){
-        return (
-            )
-        }*/
        return <Datum value={`2219.5 tons`} title={`2219.5 tons`} id="2219.5 tons" />
 }
-
-// interface DatumProps {
-//     value: string | undefined
-//     title: string
-//     id: string
-// }
-
-// const Datum = memo<DatumProps>(function _Datum({value, title, id}: DatumProps) {
-//     const special: any = isSpecial(value)
-
-//     return (
-//         <>
-//         <span key={`${id}-${special}`} css={css(valueCss, special && specialCss)} aria-labelledby={id} >{value}</span>
-//       <span css={labelCss} id={id}>{title}</span>
-//         </>
-//     )
-// }
