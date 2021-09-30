b66d57affaa3f1b97dc932219628a5b3a2e6afb5
Henry Nunez <henry.nunez@clabs.co>
2021-09-30 14:40:25 -0400

    with changes

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,19 +1,7 @@
-import buildWithCeloOnDark from "src/icons/build-with-celo_dark-bg.png"
-import educateOnDark from "src/icons/educate_dark-bg.png"
-import expandReachOnDark from "src/icons/expand-reach_dark-bg.png"
 import buildingBlocks from "src/icons/Gradient_Building_Blocks.png"
 import connectedPeople from "src/icons/Gradient_Connected_People.png"
 import human from "src/icons/Gradient_Human.png"
 import speaker from "src/icons/Gradient_Speaker.png"
 import speechBubbles from "src/icons/Gradient_Speech_Bubbles.png"
 
-export {
-  buildWithCeloOnDark,
-  educateOnDark,
-  expandReachOnDark,
-  buildingBlocks,
-  connectedPeople,
-  human,
-  speaker,
-  speechBubbles,
-}
+export { buildingBlocks, connectedPeople, human, speaker, speechBubbles }
