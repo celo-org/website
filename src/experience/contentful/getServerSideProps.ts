e79d4a0f1030285bf92412ef8e9e131e08a39328
Aaron DeRuvo <>
2021-10-08 12:22:51 -0700

    dup code

--- src/experience/contentful/getServerSideProps.ts
+++ src/experience/contentful/getServerSideProps.ts
@@ -77,9 +77,7 @@ const getServerSideProps: GetServerSideProps<
 }
 
 function isBogus(slug: string) {
-  return (
-    slug.length > 255 || slug.indexOf(".") >= 0 || slug.indexOf("%") >= 0 || slug.indexOf("%") >= 0
-  )
+  return slug.length > 255 || slug.indexOf(".") >= 0 || slug.indexOf("%") >= 0
 }
 
 export default getServerSideProps
