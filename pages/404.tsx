6599e9d8023834c8bbb947aa6808764dfd9b530a
michellewong793 <michellewong793@gmail.com>
2021-10-08 10:34:29 -0700

    ?? to ||

--- pages/404.tsx
+++ pages/404.tsx
@@ -6,7 +6,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
-      ...(await serverSideTranslations(locale ?? 'en', [NameSpaces.common])),
+      ...(await serverSideTranslations(locale || 'en', [NameSpaces.common])),
     },
   }
 }
