ed1c9379f8e65e0be01dfd717707155a6d25df24
Henry Nunez <henry.nunez@clabs.co>
2021-10-22 14:17:47 -0400

    changed the link on the apple testflight link"

--- src/shared/menu-items.ts
+++ src/shared/menu-items.ts
@@ -217,7 +217,7 @@ export enum CeloLinks {
   linkedIn = "https://www.linkedin.com/company/celoOrg/",
   blockChainRepo = "https://github.com/celo-org/celo-blockchain",
   playStoreDevWallet = "https://play.google.com/apps/testing/org.celo.mobile.alfajores",
-  appStoreDevWallet = "https://testflight.apple.com/join/s212x3Rp",
+  appStoreDevWallet = "https://testflight.apple.com/join/8njpPy3g",
   privacyDocs = "https://docs.celo.org/celo-codebase/protocol/identity/phone-number-privacy",
   tutorial = "https://docs.celo.org/getting-started/faucet#creating-an-empty-account-with-the-celo-client",
   buildWalletDocs = "https://docs.celo.org/celo-codebase/wallet/intro",
