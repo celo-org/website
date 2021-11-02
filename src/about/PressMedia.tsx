a954aaf21860c9353122df046ca80625f610725c
michellewong793 <michellewong793@gmail.com>
2021-11-02 14:01:07 -0700

    press text

--- src/about/PressMedia.tsx
+++ src/about/PressMedia.tsx
@@ -3,14 +3,19 @@ import { I18nProps, NameSpaces, withNamespaces } from "src/i18n"
 import BookLayout from "src/layout/BookLayout"
 import Button, { BTN } from "src/shared/Button.3"
 import { fonts } from "src/estyles"
+import { css } from "@emotion/react"
 
 export default withNamespaces(NameSpaces.about)(function PressAndMedia({ t }: I18nProps) {
   return (
     <BookLayout label={t("Press and Media")} endBlock={true}>
-      <p css={[fonts.body]}>
+      <span css={[fonts.body, press]}>
         {t("pressText")}{" "}
         <Button kind={BTN.INLINE} text={"press@celo.org"} href="mailto:press@celo.org" />.
-      </p>
+      </span>
     </BookLayout>
   )
+})
+
+const press = css({
+  a: fonts.body
 })
