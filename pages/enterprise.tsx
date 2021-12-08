7f86379d0fc5c0942f478b198482126320852f73
michellewong793 <michellewong793@gmail.com>
2021-12-08 10:36:26 -0800

    add enterprise page

---
+++ pages/enterprise.tsx
@@ -0,0 +1,21 @@
+import page from "src/public-sector/CommonContentFullPage"
+import { getPageBySlug } from "src/utils/contentful"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import { NameSpaces } from "src/i18n"
+
+export default page
+
+export async function getServerSideProps({ locale }) {
+  const page = await getPageBySlug("enterprise", { locale: "en-US" }, true)
+
+  if (!page) {
+    return { notFound: true }
+  }
+
+  return {
+    props: {
+      ...page,
+      ...(await serverSideTranslations(locale, [NameSpaces.common, NameSpaces.terms])),
+    },
+  }
+}
