1155b1fe0d32faa6f0988e801848afd8323f4a66
Henry Nunez <henry.nunez@clabs.co>
2021-12-17 12:20:10 -0500

    testing something out

--- server/Alliance.ts
+++ server/Alliance.ts
@@ -98,10 +98,15 @@ export function normalize(asset: Fields): Ally {
 
 //this is the normalizeHubspot from Henry
 export function normalizeHubspot(asset: HubSpotField): Ally {
+  console.log(asset.properties.logo, "this is logo")
+  let logoImage = asset.properties.logo
+  if (logoImage === null) {
+    logoImage = "https://f.hubspotusercontent10.net/hubfs/8568019/logos/a16z.png"
+  }
   return {
     name: asset.properties.name,
     url: asset.properties.domain,
-    logo: { uri: asset.properties.logo, width: 0, height: 0 },
+    logo: { uri: logoImage, width: 0, height: 0 },
     category: asset.properties.categories,
   }
 }
