0cc077de9610443cd2e3d901e476897ace99c7e2
Henry Nunez <henry.nunez@clabs.co>
2022-02-10 11:12:57 -0500

    button changes

--- src/contentful/nodes/embeds/BUTTON.tsx
+++ src/contentful/nodes/embeds/BUTTON.tsx
@@ -2,17 +2,15 @@ import * as React from "react"
 import Button from "src/shared/Button.3"
 import { Entry } from "contentful"
 import { ContentfulButton } from "src/utils/contentful"
-import { useScreenSize } from "src/layout/ScreenSize"
 
 export const BUTTON = {
-  Button: ({ fields }: Entry<ContentfulButton>) => {
-    const { isMobile } = useScreenSize()
+  button: ({ fields }: Entry<ContentfulButton>) => {
     return (
       <Button
         text={fields.words}
         href={fields.href || fields.assetLink?.fields?.file?.url}
         kind={fields.kind}
-        size={isMobile && fields.mobileSize ? fields.mobileSize : fields.size}
+        size={fields.size}
         align={fields.align}
         target={
           fields.assetLink?.fields?.file?.url || (fields.href?.startsWith("http") && "_blank")
