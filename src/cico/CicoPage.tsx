736719a3cb05b7e4aa3794c852988863ebdecf92
Henry Nunez <henry.nunez@clabs.co>
2022-03-17 14:16:20 -0400

    idk

--- src/cico/CicoPage.tsx
+++ src/cico/CicoPage.tsx
@@ -1,21 +1,21 @@
 import * as React from "react"
 import { I18nProps, NameSpaces, withNamespaces } from "src/i18n"
 
-export interface CicoProvider {
-  restricted?: string
-  population?: string
-  country: string
-  "CICO Provider": string
-  "Payment Type": string[]
-  "CICO Type": string
-  "Celo Assets": string[]
-  "Security Review": string
-  "Legal Review": string
-  "Usability Review": string
-  paid: string
-}
+// export interface CicoProvider {
+//   restricted?: string
+//   population?: string
+//   country: string
+//   'CICO Provider': string,
+//   'Payment Type': string[],
+//   'CICO Type': string,
+//   'Celo Assets': string[],
+//   'Security Review': string,
+//   'Legal Review': string,
+//   'Usability Review': string,
+//   paid: string
+// }
 
-function CicoPage(props: I18nProps & CicoProvider) {
+function CicoPage(props: I18nProps) {
   console.log(props, "this is props")
   return (
     <div>
