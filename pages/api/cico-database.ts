16122359f3dc54e810b745eefe4379df252c4d92
Henry Nunez <henry.nunez@clabs.co>
2022-03-17 14:12:49 -0400

    set up interface for CicoProvider

--- pages/api/cico-database.ts
+++ pages/api/cico-database.ts
@@ -6,7 +6,6 @@ async function get(_: NextApiRequest, res: NextApiResponse) {
   try {
     const data = await fetchCico()
     res.json(data)
-    console.log(res.json(data), "this is res")
   } catch {
     res.status(500).json([])
   }
--- src/cico/CicoPage.tsx
+++ src/cico/CicoPage.tsx
@@ -1,11 +1,25 @@
 import * as React from "react"
 import { I18nProps, NameSpaces, withNamespaces } from "src/i18n"
 
-function CicoPage(props: I18nProps) {
+export interface CicoProvider {
+  restricted?: string
+  population?: string
+  country: string
+  "CICO Provider": string
+  "Payment Type": string[]
+  "CICO Type": string
+  "Celo Assets": string[]
+  "Security Review": string
+  "Legal Review": string
+  "Usability Review": string
+  paid: string
+}
+
+function CicoPage(props: I18nProps & CicoProvider) {
   console.log(props, "this is props")
   return (
     <div>
-      <h1>hello world</h1>
+      <h1></h1>
     </div>
   )
 }
