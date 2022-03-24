386f44595662bff8c46344e87ac6aea0fdbb4b26
Henry Nunez <henry.nunez@clabs.co>
2022-03-24 18:58:51 -0400

    changed interfaces values

--- src/cico/CicoPage.tsx
+++ src/cico/CicoPage.tsx
@@ -5,13 +5,14 @@ import { containerCss } from "src/press/PressPage"
 import Chevron, { Direction } from "src/icons/chevron"
 import { colors } from "src/colors"
 import { buttonCss } from "src/contentful/grid2-cells/Playlist"
+import { IndexedData } from "@hubspot/api-client/lib/codegen/cms/site_search/api"
 
 export interface CicoProvider {
   restricted?: string
-  population?: string
+  population?: number
   country?: string
   cicoProvider?: string
-  paymentType?: string[]
+  paymentType?: PaymentType
   cicoType?: string
   celoAssets?: CeloAssets
   securityReview?: string
@@ -22,12 +23,17 @@ export interface CicoProvider {
 interface CeloAssets {
   assets: string[]
 }
+interface PaymentType {
+  payment: string[]
+}
 
 interface Props {
   data: CicoProvider[]
 }
 
 function CicoPage(props: I18nProps & Props) {
+  const [expandedIndex, setBlurbIndex] = React.useState(null)
+  const toggle = (num: number) => (expandedIndex === num ? setBlurbIndex(null) : setBlurbIndex(num))
   const { data } = props
   const byCountries = data.reduce((countries, provider) => {
     const country = provider.country
@@ -35,48 +41,48 @@ function CicoPage(props: I18nProps & Props) {
     countries[country].push(provider)
     return countries
   }, {})
-
   return (
     <div css={containerCss}>
       {Object.keys(byCountries)
         .sort()
-        .map((title) => {
+        .map((title, index) => {
           return (
             <>
               <div css={headerContainer}>
                 <h1>{title}</h1>
-                <button css={buttonCss}>
+                <button css={buttonCss} onClick={() => toggle(index)}>
                   <Chevron color={colors.greenUI} direction={Direction.down} />
                 </button>
               </div>
-              <div>
+              <div css={expandedIndex === index ? toggleContent : displayNone}>
                 <table css={countriesTable}>
                   <thead css={countriesHeader}>
                     <tr>
                       <th css={countriesHeader}>CICO Provider</th>
                       <th css={countriesHeader}>CICO Type</th>
-                      <th css={countriesHeader}>Restricted</th>
-                      <th css={countriesHeader}>Paid</th>
                       <th css={countriesHeader}>Celo Assets</th>
+                      <th css={countriesHeader}>Payment Type</th>
+                      <th css={countriesHeader}>Restricted</th>
                     </tr>
                   </thead>
                   {byCountries[title].map(
                     (country: {
                       restricted: string
-                      paid: string
                       cicoProvider: string
                       cicoType: string
                       celoAssets: CeloAssets
+                      population?: number
+                      paymentType?: PaymentType
                     }) => {
-                      debugger
                       return (
                         <>
-                          <Countries
+                          <CicoProvider
                             restricted={country.restricted}
-                            paid={country.paid}
                             cicoProvider={country.cicoProvider}
                             cicoType={country.cicoType}
                             celoAssets={country.celoAssets}
+                            population={country.population}
+                            paymentType={country.paymentType}
                           />
                         </>
                       )
@@ -91,30 +97,41 @@ function CicoPage(props: I18nProps & Props) {
   )
 }
 
-export function Countries({ restricted, paid, cicoProvider, cicoType, celoAssets }: CicoProvider) {
+export function CicoProvider({
+  restricted,
+  paid,
+  cicoProvider,
+  cicoType,
+  celoAssets,
+  population,
+  paymentType,
+}: CicoProvider) {
+  const newPopulation = !population ? "N/A" : population.toLocaleString("en-US")
   return (
-    <tbody>
-      <tr>
-        <td css={countriesBody}>{!cicoProvider ? "N/A" : cicoProvider}</td>
-        <td css={countriesBody}>{!cicoType ? "N/A" : cicoType}</td>
-        <td css={countriesBody}>{!restricted ? "N/A" : restricted}</td>
-        <td css={countriesBody}>{!paid ? "N/A" : paid}</td>
-        <td css={countriesBody}>{!celoAssets ? "N/A" : celoAssets}</td>
-      </tr>
-    </tbody>
+    <>
+      <tbody>
+        <tr>
+          <td css={countriesBody}>{!cicoProvider ? "N/A" : cicoProvider}</td>
+          <td css={countriesBody}>{!cicoType ? "N/A" : cicoType}</td>
+          <td css={countriesBody}>{!celoAssets ? "N/A" : celoAssets}</td>
+          <td css={countriesBody}>{!paymentType ? "N/A" : paymentType}</td>
+          <td css={countriesBody}>{!restricted ? "N/A" : restricted}</td>
+        </tr>
+      </tbody>
+    </>
   )
 }
 
-function ToggleButton() {
-  const [open, setOpen] = React.useState(false)
-  const toggleSort = () => (open === true ? setOpen(false) : setOpen(true))
-  return (
-    <button onClick={toggleSort}>
-      {" "}
-      <Chevron color={colors.dark} direction={open ? Direction.up : Direction.down} />{" "}
-    </button>
-  )
-}
+// function ToggleButton() {
+//   const [open, setOpen] = React.useState(false)
+//   const toggleSort = () => (open === true ? setOpen(false) : setOpen(true))
+//   return (
+//     <button onClick={toggleSort}>
+//       {" "}
+//       <Chevron color={colors.dark} direction={open ? Direction.up : Direction.down} />{" "}
+//     </button>
+//   )
+// }
 
 const countriesTable = css({
   border: "1px solid black",
@@ -131,6 +148,12 @@ const countriesBody = css(countriesCells, {
 })
 const headerContainer = css({
   display: "flex",
+})
+const toggleContent = css({
+  display: "flex",
+})
+const displayNone = css({
+  display: "none",
 })
 
 export default withNamespaces(NameSpaces.common)(CicoPage)
