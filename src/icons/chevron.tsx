45ef68b2b0db9fe468465d40d3ea8f6893a01929
Henry Nunez <henry.nunez@clabs.co>
2022-05-23 10:03:54 -0400

    added css prop on svg

--- src/icons/chevron.tsx
+++ src/icons/chevron.tsx
@@ -18,26 +18,26 @@ interface Props {
 export default class Chevron extends React.PureComponent<Props> {
   static defaultProps = {
     size: 14,
+    direction: Direction.right,
   }
   render() {
     return (
-      <g transform={`rotate(${this.props.direction})`}>
-        <svg
-          width={this.props.size}
-          height={this.props.size}
-          viewBox="0 0 8 12"
-          fill="none"
-          transform={`rotate(${this.props.direction})`}
-        >
-          <path
-            opacity={this.props.opacity || 1}
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M1.19997 0L1.13249e-05 1.19996L4.80011 6.00006L0 10.8002L1.19996 12.0001L7.19977 6.00032L7.19953 6.00007L7.19979 5.99981L1.19997 0Z"
-            fill={this.props.color}
-          />
-        </svg>
-      </g>
+      <svg
+        width={this.props.size}
+        height={this.props.size}
+        viewBox="0 0 8 12"
+        fill="none"
+        transform={`rotate(${this.props.direction})`}
+        css={`rotate(${this.props.direction})`}
+      >
+        <path
+          opacity={this.props.opacity || 1}
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M1.19997 0L1.13249e-05 1.19996L4.80011 6.00006L0 10.8002L1.19996 12.0001L7.19977 6.00032L7.19953 6.00007L7.19979 5.99981L1.19997 0Z"
+          fill={this.props.color}
+        />
+      </svg>
     )
   }
 }
